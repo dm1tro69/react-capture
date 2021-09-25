e0df2b5f4a5e119504ed0a895cdeec4ceddb4ac0
dm1tro69 <dimolg22@gmail.com>
2021-09-25 21:45:43 +0300

    add styled component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import AboutUs from "./pages/AboutUs";
+import GlobalStyle from "./components/GlobalStyle";
 
 
 function App() {
   return (
     <div className="App">
+        <GlobalStyle/>
         <AboutUs/>
     </div>
   );
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,29 +1,36 @@
 import React from 'react';
 import home1 from '../img/home1.png'
+import styled from "styled-components";
+import {About, Hide, Image, Description} from "../styles";
+
 
 const AboutSection = () => {
     return (
-        <div>
-            <div className="description">
+        <About>
+            <Description>
                 <div className="title">
-                    <div className="hide">
+                    <Hide>
                         <h2>We work to make</h2>
-                    </div>
-                    <div className="hide">
+                    </Hide>
+                    <Hide >
                         <h2>your <span>dreams</span> come</h2>
-                    </div>
-                    <div className="hide">
+                    </Hide>
+                    <Hide>
                         <h2>true.</h2>
-                    </div>
+                    </Hide>
                 </div>
                 <p>Contact us for any photography or videography ideas that you have. We have professionals with amazing skills</p>
                 <button>Contacts Us</button>
-            </div>
-            <div className="image">
+            </Description>
+            <Image>
                 <img src={home1} alt="img"/>
-            </div>
-        </div>
+            </Image>
+        </About>
     );
 };
+
+
+
+
 
 export default AboutSection;
