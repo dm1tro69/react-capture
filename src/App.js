47512f6b3462a4e9fd96b0f3d43908b7c14652a9
dm1tro69 <dimolg22@gmail.com>
2021-09-25 23:02:39 +0300

    add router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 
 import AboutUs from "./pages/AboutUs";
 import GlobalStyle from "./components/GlobalStyle";
+import Nav from "./components/Nav";
+import {Route, Switch} from "react-router-dom"
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
 
 
 function App() {
   return (
     <div className="App">
         <GlobalStyle/>
-        <AboutUs/>
+        <Nav/>
+        <Switch>
+            <Route exact path="/" ><AboutUs/></Route>
+            <Route  path="/contact"><ContactUs/></Route>
+            <Route  path="/work" ><OurWork/></Route>
+        </Switch>
+
     </div>
   );
 }
